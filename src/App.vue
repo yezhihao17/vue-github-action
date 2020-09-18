895afad6c9976b801739b6c156f3730e08fa9771
yezhihao17 <yezhihao@haiwang.link>
2020-09-18 15:26:25 +0800

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <h3>Helle Github Actions</h3>
+    <h3>Helle Github Actions </h3>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
